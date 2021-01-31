e46db1ba144f6548f035ae504bd118550ecb80ca
kelliaUmuhire <kellumuhire@gmail.com>
2021-01-31 15:21:16 +0200

    fixing issues

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,4 @@
 const db = require("../models");
-
-// mongodb+srv://admin:amdin@2020@cluster0.1n16l.mongodb.net/bosses-ecommerce?retryWrites=true&w=majority
-// mongodb+srv://kellme:letmein3@cluster0.lx7ml.mongodb.net/Ecomm?retryWrites=true&w=majority
 let MONGO_URI =
   "mongodb+srv://admin:amdin@2020@cluster0.1n16l.mongodb.net/bosses-ecommerce?retryWrites=true&w=majority";
 
