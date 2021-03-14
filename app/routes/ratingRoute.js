cf1ae9d3f0d5170e2423c731bf20aed5add4ae0f
Blessing Hirwa <hirwablessing7@gmail.com>
2021-03-14 15:43:31 +0200

    update routing

--- app/routes/ratingRoute.js
+++ app/routes/ratingRoute.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const RateController = require('../controllers/rateController')
-const { verifyToken } = require('../middlewares/authJwt')
-const router = express.Router()
+const express = require("express");
+const RateController = require("../controllers/RateController");
+const { verifyToken } = require("../middlewares/authJwt");
+const router = express.Router();
 
-router.post('/rate', [verifyToken, RateController.RateProduct])
-router.get('/', [verifyToken, RateController.getRates])
-router.get('/:stars', [verifyToken,RateController.getRateByStars])
-router.delete('/:id', [verifyToken, RateController.unrate])
-router.put('/:id', [verifyToken, RateController.updateRate])
+router.post("/rate", [verifyToken, RateController.RateProduct]);
+router.get("/", [verifyToken, RateController.getRates]);
+router.get("/:stars", [verifyToken, RateController.getRateByStars]);
+router.delete("/:id", [verifyToken, RateController.unrate]);
+router.put("/:id", [verifyToken, RateController.updateRate]);
 
-module.exports = router
+module.exports = router;
